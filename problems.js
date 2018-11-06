508022e5838e6e371063f2c6b4a5ff7ac0cda137
JohnLyetacker <44590586+JohnLyetacker@users.noreply.github.com>
2018-11-07 01:32:11 +0300

    min

--- problems.js
+++ problems.js
@@ -1,6 +1,8 @@
 "use strict";
-var i=0;
-while (i<5) {
-	alert(i);
-	i++
+function min(x, y) {
+	return x < y ? x : y;
 }
+var x = prompt('x','x');
+var y = prompt('y','y');
+var z = min(x,y);
+alert(z);
