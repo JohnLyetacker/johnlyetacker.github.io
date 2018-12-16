b12fcd29c4e84b38922c255cb3fd4e7c83029373
JohnLyetacker <44590586+JohnLyetacker@users.noreply.github.com>
2018-12-16 19:39:30 +0300

    secondPage

--- problems.js
+++ problems.js
@@ -1,12 +1,4 @@
 "use strict";
-function min(x, y) {
-	if (x > y) {
-		return x;
-	} else {
-		return y;
-	}
-}
-var x = prompt('x','x');
-var y = prompt('y','y');
-var z = min(x,y);
-alert(z);
+
+var date = new Date ();
+alert(date);
---
+++ random/randomdick.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var dick = ['Чёрный', 'Азиатский', 'Европейский', 'Мексиканский', 'Классический'];
+var rand = Math.floor(Math.random() * dick.length);
+alert('Ваш хуй - ' + dick[rand]);
