1e66d2709774c090aa8472da33b65c80180fc48d
JohnLyetacker <44590586+JohnLyetacker@users.noreply.github.com>
2019-09-24 15:23:46 +0300

    new dicks

--- random/randomdick.js
+++ random/randomdick.js
@@ -1,11 +1,31 @@
 // "use straight";
-var dick = ['чёрный',
+let dick = ['чёрный',
     'азиатский',
     'европейский',
     'мексиканский',
     'классический',
-    "взрывнаяАрабика",
+    'взрывнаяАрабика',
     'маленький',
-    "нанохуй"];
-var rand = Math.floor(Math.random() * dick.length);
-document.getElementById("getDick").childNodes[0].nodeValue = dick[rand];
+    'нанохуй',
+    'покрашенный в грим клоунский',
+    'пиратский протез',
+    'залупа откушена собакой',
+];
+let rand = Math.floor(Math.random() * dick.length);
+let dickLength = Math.floor(Math.random() * 5) + 1;
+let cm = " ";
+switch (dickLength) {
+    case 1: cm = " сантиметр";
+        break;
+    case 2: cm = " сантиметра";
+        break;
+    case 3: cm = " сантиметра";
+        break;
+    case 4: cm = " сантиметра";
+        break;
+    case 5: cm = " сантиметров";
+        break;
+    default: cm = " error";
+        break;
+}
+document.getElementById("getDick").childNodes[0].nodeValue = dick[rand] + ", длина - " + dickLength + cm;
